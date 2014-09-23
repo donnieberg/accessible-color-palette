0ab2eef54bd9728a4a1cdcefeec3acce9dabeeb5
Donielle Berg <d.berg@salesforce.com>
2014-09-23 15:49:12 -0700

    Add more hard coded colors

--- app.js
+++ app.js
@@ -1,50 +1,65 @@
 var app = angular.module('app', []);
 
 app.controller('appController', function($scope, $http) {
-  $scope.fontSize = null;
+  /**
+   * Defaults
+   */
+  $scope.userContent = 'The quick brown fox jumps over the lazy dog.';
+  $scope.fontFamily = null;
+  $scope.fontSize = 18;
+  $scope.fontWeight = 400;
+  $scope.backgroundColor = '#fff';
   $scope.WCAGlevel = 'AA';
   $scope.colorCategories = [
-  { hex: '#006eb3', name: 'blue', className: 'bg-1' },
-  { hex: '#be554b', name: 'red', className: 'bg-2' },
-  { hex: '#e6b739', name: 'yellow', className: 'bg-3' },
-  { hex: '#3c3d3e', name: 'black', className: 'bg-4' },
-  { hex: '#696e71', name: 'gray-dark', className: 'bg-5' },
-  { hex: '#afb5b9', name: 'gray-lt', className: 'bg-6' }
+  { pass: true, hex: '#16A085', rgb: '', name: 'green-drk', className: 'bg-1' },
+  { pass: true, hex: '#2ECC71', rgb: '', name: 'green-lt', className: 'bg-2' },
+  { pass: true, hex: '#3498DB', rgb: '', name: 'blue', className: 'bg-3' },
+  { pass: true, hex: '#9B59B6', rgb: '', name: 'purple', className: 'bg-4' },
+  { pass: true, hex: '#34495E', rgb: '', name: 'black', className: 'bg-5' },
+  { pass: true, hex: '#F2CA27', rgb: '', name: 'yellow', className: 'bg-6' },
+  { pass: true, hex: '#E67E22', rgb: '', name: 'orange', className: 'bg-7' },
+  { pass: true, hex: '#E74C3C', rgb: '', name: 'red', className: 'bg-8', colorVariations: [
+    { pass: true, hex: '#D24D57', rgb: '', name: 'chestnut-rose', className: 'chestnut-rose' },
+    { pass: true, hex: '#F22613', rgb: '', name: 'pomegranate', className: 'pomegranate' },
+    { pass: true, hex: '#FF0000', rgb: '', name: 'red', className: 'red' },
+    { pass: true, hex: '#D91E18', rgb: '', name: 'thunderbird', className: 'thunderbird' },
+    { pass: true, hex: '#96281B', rgb: '', name: 'old-brick', className: 'old-brick' },
+    { pass: true, hex: '#EF4836', rgb: '', name: 'flamingo', className: 'flamingo' },
+    { pass: true, hex: '#D64541', rgb: '', name: 'valencia', className: 'valencia' },
+    { pass: true, hex: '#C0392B', rgb: '', name: 'tall-poppy', className: 'tall-poppy' },
+    { pass: true, hex: '#CF000F', rgb: '', name: 'monza', className: 'monza' },
+    { pass: true, hex: '#E74C3C', rgb: '', name: 'cinnabar', className: 'cinnabar' }
+  ]
+  },
+  { pass: true, hex: '#ECF0F1', rgb: '', name: 'gray-lt', className: 'bg-9' },
+  { pass: true, hex: '#95A5A6', rgb: '', name: 'gray-drk', className: 'bg-10' }
+  ];
+
+  $scope.reds = [
+  { pass: true, hex: '#D24D57', rgb: '', name: 'chestnut-rose', className: 'chestnut-rose' },
+  { pass: true, hex: '#F22613', rgb: '', name: 'pomegranate', className: 'pomegranate' },
+  { pass: true, hex: '#FF0000', rgb: '', name: 'red', className: 'red' },
+  { pass: true, hex: '#D91E18', rgb: '', name: 'thunderbird', className: 'thunderbird' },
+  { pass: true, hex: '#96281B', rgb: '', name: 'old-brick', className: 'old-brick' },
+  { pass: true, hex: '#EF4836', rgb: '', name: 'flamingo', className: 'flamingo' },
+  { pass: true, hex: '#D64541', rgb: '', name: 'valencia', className: 'valencia' },
+  { pass: true, hex: '#C0392B', rgb: '', name: 'tall-poppy', className: 'tall-poppy' },
+  { pass: true, hex: '#CF000F', rgb: '', name: 'monza', className: 'monza' },
+  { pass: true, hex: '#E74C3C', rgb: '', name: 'cinnabar', className: 'cinnabar' }
   ];
 
 
   /**
-   * Calculate Ratio based on foreground and background
-   * @param {string} accpets two string values to calculate ratio. Strings can be either hex or rgb.
-   * @return contrast ratio
+   * Change in WCAG Level needs a recalc (after user clicks on different ratio button)
    */
-  $scope.getContrastRatio = function(foreground, background) {
-    return contrastRatio(foreground, background);
+  $scope.getRatio = function() {
+    $scope.getCurrentRatio();
+    $scope.getPassingColors();
   };
 
   /**
-   * @param {Number} ratio the contrast ratio between two colors
-   * @param {Object} view the current view mode in the tool
-   * @returns {Boolean} whether or not the given contrast ratio
-   * passes the given view's required contrast ratio
+   * Calculate Current Ratio based on user inputs for font size and WCGAG Level AA or AAA
    */
-  $scope.passes = function(ratio, view) {
-    return ratio >= view.ratio;
-  };
-
-  $scope.getRatio_fsChange = function(keyEvent) {
-    if(keyEvent.keyCode === 13){
-      $scope.getCurrentRatio();
-      _.each($scope.colorCategories, function(num) {
-        console.log(num);
-      })
-    }
-  };
-
-  $scope.getRatio_levelChange = function() {
-      $scope.getCurrentRatio();
-  };
-
   $scope.getCurrentRatio = function() {
     var currentFS = $scope.fontSize;
     var currentLevel = $scope.WCAGlevel;
@@ -54,6 +69,26 @@ app.controller('appController', function($scope, $http) {
       currentLevel === 'AA' ? $scope.currentRatio = 3.1 : $scope.currentRatio = 4.5;
     }
     //console.log('the current ratio is: ', $scope.currentRatio);
+  };
+
+  /**
+   * Calculate Passing Colors to user Current Ratio by comparing set foreground colors and user's background color
+   * @param {string} accpets two string values to calculate ratio. Strings can be either hex or rgb.
+   */
+  $scope.getPassingColors = function() {
+    _.each($scope.colorCategories, function(color) {
+      var ratio = contrastRatio(color.hex, $scope.backgroundColor);
+      ratio >= $scope.currentRatio ? color.pass = true : color.pass = false;
+      color.pass === true ? color.currentRatio = ratio : undefined;
+    })
+  };
+
+  /**
+   * When user clicks on color, make user content that color
+   */
+  $scope.setTextColor = function(color) {
+    $scope.currentColor = color;
+    console.log($scope.currentColor);
   };
 
 
@@ -123,3 +158,14 @@ app.controller('appController', function($scope, $http) {
   }
 
 });
+/*
+ *
+ * Change in Font Size needs a recalc (after updates input and hits enter)
+  $scope.getRatio_onEnter = function(keyEvent) {
+    if(keyEvent.keyCode === 13){
+      $scope.getCurrentRatio();
+      $scope.getPassingColors();
+    }
+  };
+
+  */
