4388697bda58f29a17c22e32b59372948f431b74
Donielle Berg <d.berg@salesforce.com>
2014-09-26 11:31:27 -0700

    Fix bugs and return passing colors comprised of hard coded from flatUI website and dynamically generated shades

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.controller('appController', function($scope, $http, appFactory) {
   $scope.fontWeight = 400;
   $scope.backgroundColor = '#ffffff';
   $scope.WCAGlevel = 'AA';
+  $scope.appFactory = appFactory;
 
 
   /**
@@ -45,7 +46,7 @@ app.controller('appController', function($scope, $http, appFactory) {
       color.currentRatio = ratio;
       ratio >= $scope.currentRatio ? color.pass = true : color.pass = false;
     })
-    console.log('the current color variations are: ', $scope.currentColor.colorVariations);
+    //console.log('the current color variations are: ', $scope.currentColor.colorVariations);
   };
 
   var changeShade = function(col, amt) {
@@ -69,33 +70,15 @@ app.controller('appController', function($scope, $http, appFactory) {
 
   $scope.getColorVariations = function() {
     $scope.currentColor.colorVariations = [];
+    $scope.currentColor.colorVariations = $scope.currentColor.flatUIcolors;
     var darkestShade = changeShade($scope.currentColor.hex, -45);
     var percentage = 10;
     for(var i=0; i<25; i++){
       percentage += 5;
       var newShade = changeShade(darkestShade, percentage);
       $scope.currentColor.colorVariations.push({ hex: newShade });
-    }
-  }
-
-  //$scope.getColorVariations = function() {
-  //  $scope.currentColor.colorVariations = [];
-  //  var strVariations = $scope.currentColor.rgb.split(",");
-  //  var intVariations = _.map(strVariations, function(num) {
-  //    return parseInt(num);
-  //  });
-  //  var r = intVariations[0]%256;
-  //  var g = intVariations[1]%256;
-  //  var b = intVariations[2]%256;
-  //  for(var i=0;i<10;i++){
-  //    r+=5;
-  //    g+=5;
-  //    b+=5;
-  //    $scope.currentColor.colorVariations.push({ pass: true, currentRatio: null, rgb: "rgb("+r+","+g+","+b+")"});
-  //  }
-  //  //console.log('the current color variations are: ', $scope.currentColor.colorVariations);
-  //};
-
+    };
+  };
 
   /**
    * When user clicks on color variation, make user text that color
@@ -172,20 +155,129 @@ app.controller('appController', function($scope, $http, appFactory) {
   }
 
 });
-/*
- *
- * Change in Font Size needs a recalc (after updates input and hits enter)
-  $scope.getRatio_onEnter = function(keyEvent) {
-    if(keyEvent.keyCode === 13){
-      $scope.getCurrentRatio();
-      $scope.getPassingColors();
-    }
-  };
-
-  */
 
 app.factory('appFactory', function() {
   return {
+    colorCategories: [
+      //{ hex: '#16A085', rgb: '22, 160, 133', name: 'green-drk', colorVariations: []  },
+      { hex: '#2ECC71', rgb: '46, 204, 113', name: 'green-lt', shades: [], flatUIcolors: [
+        { pass: true, hex: '#4ECDC4', rgb: '', name: '' },
+        { pass: true, hex: '#A2DED0', rgb: '', name: '' },
+        { pass: true, hex: '#87D37C', rgb: '', name: '' },
+        { pass: true, hex: '#90C695', rgb: '', name: '' },
+        { pass: true, hex: '#26A65B', rgb: '', name: '' },
+        { pass: true, hex: '#03C9A9', rgb: '', name: '' },
+        { pass: true, hex: '#68C3A3', rgb: '', name: '' },
+        { pass: true, hex: '#65C6BB', rgb: '', name: '' },
+        { pass: true, hex: '#1BBC9B', rgb: '', name: '' },
+        { pass: true, hex: '#1BA39C', rgb: '', name: '' },
+        { pass: true, hex: '#66CC99', rgb: '', name: '' },
+        { pass: true, hex: '#36D7B7', rgb: '', name: '' },
+        { pass: true, hex: '#C8F7C5', rgb: '', name: '' },
+        { pass: true, hex: '#86E2D5', rgb: '', name: '' },
+        { pass: true, hex: '#2ECC71', rgb: '', name: '' },
+        { pass: true, hex: '#16A085', rgb: '', name: '' },
+        { pass: true, hex: '#3FC380', rgb: '', name: '' },
+        { pass: true, hex: '#019875', rgb: '', name: '' },
+        { pass: true, hex: '#03A678', rgb: '', name: '' },
+        { pass: true, hex: '#4DAF7C', rgb: '', name: '' },
+        { pass: true, hex: '#2ABB9B', rgb: '', name: '' },
+        { pass: true, hex: '#00B16A', rgb: '', name: '' },
+        { pass: true, hex: '#1E824C', rgb: '', name: '' },
+        { pass: true, hex: '#049372', rgb: '', name: '' },
+        { pass: true, hex: '#26C281', rgb: '', name: '' }
+        ]
+      },
+      { hex: '#3498DB', rgb: '52, 152, 219', name: 'blue', shades: [], flatUIcolors: [
+        { pass: true, hex: '#E4F1FE', rgb: '', name: 'alice-blue' },
+        { pass: true, hex: '#4183D7', rgb: '', name: 'royal-blue' },
+        { pass: true, hex: '#59ABE3', rgb: '', name: 'picton-blue' },
+        { pass: true, hex: '#81CFE0', rgb: '', name: 'spray' },
+        { pass: true, hex: '#52B3D9', rgb: '', name: 'shakespeare' },
+        { pass: true, hex: '#C5EFF7', rgb: '', name: '' },
+        { pass: true, hex: '#22A7F0', rgb: '', name: '' },
+        { pass: true, hex: '#3498DB', rgb: '', name: '' },
+        { pass: true, hex: '#2C3E50', rgb: '', name: '' },
+        { pass: true, hex: '#19B5FE', rgb: '', name: '' },
+        { pass: true, hex: '#336E7B', rgb: '', name: '' },
+        { pass: true, hex: '#22313F', rgb: '', name: '' },
+        { pass: true, hex: '#6BB9F0', rgb: '', name: '' },
+        { pass: true, hex: '#1E8BC3', rgb: '', name: '' },
+        { pass: true, hex: '#3A539B', rgb: '', name: '' },
+        { pass: true, hex: '#34495E', rgb: '', name: '' },
+        { pass: true, hex: '#67809F', rgb: '', name: '' },
+        { pass: true, hex: '#2574A9', rgb: '', name: '' },
+        { pass: true, hex: '#1F3A93', rgb: '', name: '' },
+        { pass: true, hex: '#89C4F4', rgb: '', name: '' },
+        { pass: true, hex: '#4B77BE', rgb: '', name: '' },
+        { pass: true, hex: '#5C97BF', rgb: '', name: '' }
+        ]
+      },
+      { hex: '#9B59B6', rgb: '155, 89, 182', name: 'purple', shades: [], flatUIcolors: [
+        { pass: true, hex: '#DCC6E0', rgb: '', name: 'snuff' },
+        { pass: true, hex: '#663399', rgb: '', name: 'rebecca-purple' },
+        { pass: true, hex: '#674172', rgb: '', name: 'honey-flower' },
+        { pass: true, hex: '#AEA8D3', rgb: '', name: 'wistful' },
+        { pass: true, hex: '#913D88', rgb: '', name: 'plum' },
+        { pass: true, hex: '#9A12B3', rgb: '', name: 'seance' },
+        { pass: true, hex: '#BF55EC', rgb: '', name: 'medium-purple' },
+        { pass: true, hex: '#BE90D4', rgb: '', name: 'light-wisteria' },
+        { pass: true, hex: '#8E44AD', rgb: '', name: 'studio' },
+        { pass: true, hex: '#9B59B6', rgb: '', name: 'wisteria' }
+        ]
+      },
+      //{ hex: '#34495E', rgb: '52, 73, 94', name: 'black', shades: []  },
+      { hex: '#F2CA27', rgb: '242, 202, 39', name: 'yellow', shades: [], flatUIcolors: [
+        { pass: true, hex: '#F5D76E', rgb: '', name: '' },
+        { pass: true, hex: '#F7CA18', rgb: '', name: '' },
+        { pass: true, hex: '#F4D03F', rgb: '', name: '' }
+        ]
+      },
+      { hex: '#E67E22', rgb: '230, 126, 34', name: 'orange', shades: [], flatUIcolors: [
+        { pass: true, hex: '#FDE3A7', rgb: '', name: '' },
+        { pass: true, hex: '#F89406', rgb: '', name: '' },
+        { pass: true, hex: '#EB9532', rgb: '', name: '' },
+        { pass: true, hex: '#E87E04', rgb: '', name: '' },
+        { pass: true, hex: '#F4B350', rgb: '', name: '' },
+        { pass: true, hex: '#F2784B', rgb: '', name: '' },
+        { pass: true, hex: '#EB974E', rgb: '', name: '' },
+        { pass: true, hex: '#F5AB35', rgb: '', name: '' },
+        { pass: true, hex: '#D35400', rgb: '', name: '' },
+        { pass: true, hex: '#F39C12', rgb: '', name: '' },
+        { pass: true, hex: '#F9690E', rgb: '', name: '' },
+        { pass: true, hex: '#F9BF3B', rgb: '', name: '' },
+        { pass: true, hex: '#F27935', rgb: '', name: '' },
+        { pass: true, hex: '#E67E22', rgb: '', name: '' }
+        ]
+      },
+      { hex: '#E74C3C', rgb: '231, 76, 60', name: 'red', shades: [], flatUIcolors: [
+        { pass: true, hex: '#D24D57', rgb: '', name: 'chestnut-rose'},
+        { pass: true, hex: '#F22613', rgb: '', name: 'pomegranate'},
+        { pass: true, hex: '#FF0000', rgb: '', name: 'red'},
+        { pass: true, hex: '#D91E18', rgb: '', name: 'thunderbird'},
+        { pass: true, hex: '#96281B', rgb: '', name: 'old-brick'},
+        { pass: true, hex: '#EF4836', rgb: '', name: 'flamingo'},
+        { pass: true, hex: '#D64541', rgb: '', name: 'valencia'},
+        { pass: true, hex: '#C0392B', rgb: '', name: 'tall-poppy'},
+        { pass: true, hex: '#CF000F', rgb: '', name: 'monza'},
+        { pass: true, hex: '#E74C3C', rgb: '', name: 'cinnabar'}
+        ]
+      },
+      { hex: '#95A5A6', rgb: '149, 165, 166', name: 'gray-drk', shades: [], flatUIcolors: [
+        { pass: true, hex: '#ECECEC', rgb: '', name: '' },
+        { pass: true, hex: '#6C7A89', rgb: '', name: '' },
+        { pass: true, hex: '#D2D7D3', rgb: '', name: '' },
+        { pass: true, hex: '#EEEEEE', rgb: '', name: '' },
+        { pass: true, hex: '#BDC3C7', rgb: '', name: '' },
+        { pass: true, hex: '#ECF0F1', rgb: '', name: '' },
+        { pass: true, hex: '#95A5A6', rgb: '', name: '' },
+        { pass: true, hex: '#DADFE1', rgb: '', name: '' },
+        { pass: true, hex: '#ABB7B7', rgb: '', name: '' },
+        { pass: true, hex: '#F2F1EF', rgb: '', name: '' },
+        { pass: true, hex: '#BFBFBF', rgb: '', name: '' }
+        ]
+      }
+    ],
     flatUIcolors: {
       reds: [
           { pass: true, hex: '#D24D57', rgb: '', name: 'chestnut-rose'},
@@ -306,17 +398,6 @@ app.factory('appFactory', function() {
         { pass: true, hex: '#F2F1EF', rgb: '', name: '' },
         { pass: true, hex: '#BFBFBF', rgb: '', name: '' }
       ]
-    },
-    colorCategories: [
-      //{ hex: '#16A085', rgb: '22, 160, 133', name: 'green-drk', colorVariations: []  },
-      { hex: '#2ECC71', rgb: '46, 204, 113', name: 'green-lt', colorVariations: []  },
-      { hex: '#3498DB', rgb: '52, 152, 219', name: 'blue', colorVariations: []  },
-      { hex: '#9B59B6', rgb: '155, 89, 182', name: 'purple', colorVariations: [] },
-      { hex: '#34495E', rgb: '52, 73, 94', name: 'black', colorVariations: []  },
-      { hex: '#F2CA27', rgb: '242, 202, 39', name: 'yellow', colorVariations: []  },
-      { hex: '#E67E22', rgb: '230, 126, 34', name: 'orange', colorVariations: [] },
-      { hex: '#E74C3C', rgb: '231, 76, 60', name: 'red', colorVariations: [] },
-      { hex: '#95A5A6', rgb: '149, 165, 166', name: 'gray-drk', colorVariations: []  }
-    ]
+    }
   }
 });
