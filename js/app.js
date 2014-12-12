fa2bcc9a38ec7bfdfce50cd585002926fa0d0b54
Donielle Berg <d.berg@salesforce.com>
2014-12-12 14:59:50 -0800

    Remove ngAnimate and hex on hover

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['duScroll', 'colorpicker.module', 'ngAnimate']);
+var app = angular.module('app', ['duScroll', 'colorpicker.module']);
 
 //=============================================
 // DIRECTIVES
