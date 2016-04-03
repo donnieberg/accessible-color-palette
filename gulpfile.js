c8a9061739b31924fa82105ccae830fb8eb44e9d
Donielle Berg <donielle.berg@gmail.com>
2016-04-02 21:33:55 -0400

    Add back in underscore in bower for vendor dependency

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('js', function(){
 });
 
 gulp.task('minifyJS', function() {
-  gulp.src(['bower_components/jquery/dist/jquery.min.js', 'bower_components/angular/angular.min.js','bower_components/angular-scroll/angular-scroll.min.js', 'js/vendor/bootstrap-colorpicker-module.js', 'bower_components/tinycolor/tinycolor.js', 'bower_components/mixitup/build/jquery.mixitup.min.js'])
+  gulp.src(['bower_components/jquery/dist/jquery.min.js', 'bower_components/underscore/underscore-min.js', 'bower_components/angular/angular.min.js','bower_components/angular-scroll/angular-scroll.min.js', 'js/vendor/bootstrap-colorpicker-module.js', 'bower_components/tinycolor/tinycolor.js', 'bower_components/mixitup/build/jquery.mixitup.min.js'])
     .pipe(concat('vendorJS.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'))
